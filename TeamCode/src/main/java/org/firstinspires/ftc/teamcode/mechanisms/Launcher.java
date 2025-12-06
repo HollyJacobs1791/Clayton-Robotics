af36a06702c4105c2c7a97d9f2f70d7c1ec70cab
HollyJacobs1791 <133693373+HollyJacobs1791@users.noreply.github.com>
2025-12-06 00:37:41 -0500

    First Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Launcher.java
@@ -0,0 +1,115 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+public class Launcher {
+    private final double FEED_TIME_SECONDS = 0.20; //The feeder servos run this long when a shot is requested.
+    private final double STOP_SPEED = 0.0; //We send this power to the servos when we want them to stop.
+    private final double FULL_SPEED = 1.0;
+    private final double LAUNCHER_TARGET_VELOCITY = 1125;
+    private final double LAUNCHER_MIN_VELOCITY = 1075;
+
+    private DcMotorEx launcher;
+
+    private CRServo leftFeeder, rightFeeder;
+
+    ElapsedTime feederTimer = new ElapsedTime();
+
+    private enum LaunchState {
+        IDLE,
+        SPIN_UP,
+        LAUNCH,
+        LAUNCHING,
+    }
+
+    private LaunchState launchState;
+
+    public void init(HardwareMap hwMap){
+        launcher = hwMap.get(DcMotorEx.class, "Launcher");
+        leftFeeder = hwMap.get(CRServo.class, "left_feeder");
+        rightFeeder = hwMap.get(CRServo.class, "right_feeder");
+
+        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launcher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(
+                300, 0, 0, 10));
+        leftFeeder.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        launchState = LaunchState.IDLE;
+        stopFeeder();
+        stopLauncher();
+
+    }
+
+    public boolean isLaunching() {
+        // true while we are feeding a ball
+        return launchState == LaunchState.LAUNCHING;
+    }
+
+    public boolean isIdle() {
+        // true when we're between shots
+        return launchState == LaunchState.IDLE;
+    }
+
+    public void stopFeeder() {
+        leftFeeder.setPower(STOP_SPEED);
+        rightFeeder.setPower(STOP_SPEED);
+    }
+
+    public void updateState() {
+        switch (launchState) {
+            case IDLE:
+                break;
+            case SPIN_UP:
+                launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
+                if (launcher.getVelocity() >= LAUNCHER_MIN_VELOCITY) {
+                    //transition states
+                    launchState = LaunchState.LAUNCH;
+                }
+                break;
+            case LAUNCH:
+                leftFeeder.setPower(FULL_SPEED);
+                rightFeeder.setPower(FULL_SPEED);
+                feederTimer.reset();
+                //transition
+                launchState = LaunchState.LAUNCHING;
+                break;
+            case LAUNCHING:
+                if (feederTimer.seconds() > FEED_TIME_SECONDS) {
+                    stopFeeder();
+                    //transition event
+                    launchState = LaunchState.IDLE;
+                }
+                break;
+        }
+    }
+
+    public void startLauncher() {
+        if (launchState == LaunchState.IDLE) {
+            //transition states
+            launchState = LaunchState.SPIN_UP;
+        }
+    }
+
+    public void stopLauncher() {
+        stopFeeder();
+        launcher.setVelocity(STOP_SPEED);
+        launchState = LaunchState.IDLE;
+    }
+
+    public String getState() {
+        return launchState.toString();
+    }
+
+    public double getVelocity() {
+        return launcher.getVelocity();
+    }
+}
